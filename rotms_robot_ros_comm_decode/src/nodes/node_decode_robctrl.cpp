17b9c56c0c4f7fb61d9371b1263f5a8d12eddf72
Yihao Liu <yliu333@jhu.edu>
2022-06-06 18:38:07 -0400

    nested yaml works

--- rotms_robot_ros_comm_decode/src/nodes/node_decode_robctrl.cpp
+++ rotms_robot_ros_comm_decode/src/nodes/node_decode_robctrl.cpp
@@ -1,11 +1,17 @@
 #include <yaml-cpp/yaml.h>
+#include <ros/ros.h>
+#include <ros/package.h>
 
 int main(int argc, char **argv)
 {
-    std::string packpath = ros::package::getPath("rotms_robot_ros_comm");
+    ros::init(argc, argv, "CommDecodeRobCtrl");
+    ros::NodeHandle nh;
+
+    std::string packpath = ros::package::getPath("rotms_robot_ros_comm_decode");
 	YAML::Node f = YAML::LoadFile(packpath + "/config_comm_decode.yaml");
     YAML::Node ff = f["ROBCTRL_CMDS"];
-    std::string test = ff["GET_JNT_ANGS"]
+    std::string test = ff["GET_JNT_ANGS"].as<std::string>();
     ROS_INFO_STREAM("Msg: "<<test);
+    
     return 0;
 }
