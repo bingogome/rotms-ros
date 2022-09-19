b0af2ecd961184d184d6be94e9e021b86f0b2e04
Yihao Liu <yliu333@jhu.edu>
2022-09-18 20:18:26 -0400

    XR support

--- rotms_ros_comm/src/nodes/node_xr_comm_hif_out.cpp
+++ rotms_ros_comm/src/nodes/node_xr_comm_hif_out.cpp
@@ -22,28 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ***/
 
+#include <ros/ros.h>
+#include <ros_side_hif_out_node.hpp>
 #include <ros_side_out.hpp>
-#include <ros_side_out_node.hpp>
-#include <ros/package.h>
 
-#include <yaml-cpp/yaml.h>
-#include <boost/asio.hpp>
-
-ROSSideOutAndAck CommNodeOutAndAckIniter(ros::NodeHandle& n, std::string modulesuffix)
+int main(int argc, char **argv)
 {
-    boost::asio::io_context io_context;
-    std::string packpath = ros::package::getPath("rotms_ros_comm");
-	YAML::Node f = YAML::LoadFile(packpath + "/config_comm.yaml");
+	ros::init(argc, argv, "CommHiFOutXR");
+    ros::NodeHandle n;
 
-    struct ROSSideOutAndAckConfig cfg;
-	cfg.ip_out = f["IP_OUT_"+modulesuffix].as<std::string>();
-	cfg.port_out = f["PORT_OUT_"+modulesuffix].as<int>();
-	cfg.msg_size = f["MSG_SIZE_"+modulesuffix].as<int>();
-	cfg.subscriber_name = f["SUBSCRIBER_NAME_"+modulesuffix].as<std::string>();
-	cfg.publisher_name = f["PUBLISHER_NAME_"+modulesuffix].as<std::string>();
-	cfg.verbose = f["VERBOSE_"+modulesuffix].as<int>();
+	ROSSideOut server = CommNodeHiFOutIniter(n, "XR");
 
-	ROSSideOutAndAck server(n, io_context, cfg);
-
-	return server;
+	ros::spin();
+	return 0;
 }
